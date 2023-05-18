df3d41c961744e06f153459a4718bb4602416a2c
smctk <101852019+smctk@users.noreply.github.com>
2023-05-18 15:03:45 +0900

    Create streamlit_app.py
    add main file

---
+++ streamlit_app.py
@@ -0,0 +1,54 @@
+import openai 
+import streamlit as st
+import toml
+
+
+def show_messages(text):
+    messages_str = [
+        f"{_['role']}: {_['content']}" for _ in st.session_state["messages"][1:]
+    ]
+    text.text_area("メッセージ履歴", value=str("\n".join(messages_str)), height=400)
+
+
+st.header("GPT私設秘書")
+
+with open("secrets.toml", "r") as f:
+    config = toml.load(f)
+
+openai.api_key = config["OPENAI_KEY"]
+BASE_PROMPT = [{"role": "system", "content": "You are a helpful assistant."}]
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = BASE_PROMPT
+
+
+text = st.empty()
+show_messages(text)
+
+col3_1, col3_2 = st.columns([5,1]);
+with col3_1:
+    st.caption("chatGPTがあなたの質問に答えます。履歴を消したい場合は消去を押して下さい")
+with col3_2:
+    if st.button("消去"):
+        st.session_state["messages"] = BASE_PROMPT
+        show_messages(text)
+
+
+
+col1, col2 = st.columns([5, 1])
+with col1:
+    prompt = st.text_input("入力欄 👇", value="",placeholder="ここに質問を記入してください")
+
+with col2:
+    st.caption("送信して質問")
+    if st.button("送信"):
+        with st.spinner("メッセージを生成中..."):
+            st.session_state["messages"] += [{"role": "user", "content": prompt}]
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo", messages=st.session_state["messages"]
+            )
+            message_response = response["choices"][0]["message"]["content"]
+            st.session_state["messages"] += [
+                {"role": "system", "content": message_response}
+            ]
+            show_messages(text)
