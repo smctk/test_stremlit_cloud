750ae4a3cf56caed3294552af8ce9a7a9fc6d9ad
smctk <101852019+smctk@users.noreply.github.com>
2023-05-24 12:51:15 +0900

    Update streamlit_app.py
    Add radio button

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ st.header("GPTアシスタント")
 
 #If used with streamlitcloud
 openai.api_key = st.secrets["chatGPT"]["OPENAI_KEY"]
-BASE_PROMPT = [{"role": "system", "content": "You are a helpful assistant."}]
+BASE_PROMPT = [{"role": "system", "content": "あなたは関西弁で話す親切なおばちゃんです"}]
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = BASE_PROMPT
@@ -25,27 +25,30 @@ if "messages" not in st.session_state:
 text = st.empty()
 show_messages(text)
 
-col3_1, col3_2 = st.columns([5,1]);
-with col3_1:
+col1_1, col1_2 = st.columns([6,1]);
+with col1_1:
     st.caption("GPT3.5があなたの質問に答えます。履歴を消したい場合は消去を押して下さい")
-with col3_2:
+with col1_2:
     if st.button("消去"):
         st.session_state["messages"] = BASE_PROMPT
         show_messages(text)
 
-
-
-col1, col2 = st.columns([5, 1])
-with col1:
+col2_1, col2_2, col2_3 = st.columns([5, 1, 1])
+with col2_1:
     prompt = st.text_input("入力欄 👇", value="",placeholder="ここに質問を記入してください")
 
-with col2:
+with col2_2:
+    temp = st.radio("回答形式",("厳密", "程々", "自由"))
+    temp_dict = {"厳密":0, "程々":0.5, "自由":1}
+    Select_temp = temp_dict[temp]
+
+with col2_3:
     st.caption("送信して質問")
     if st.button("送信"):
         with st.spinner("メッセージを生成中..."):
             st.session_state["messages"] += [{"role": "user", "content": prompt}]
             response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo", messages=st.session_state["messages"], temperature=0.3, max_tokens =1024
+                model="gpt-3.5-turbo", messages=st.session_state["messages"], temperature=Select_temp, max_tokens =256
             )
             message_response = response["choices"][0]["message"]["content"]
             st.session_state["messages"] += [
